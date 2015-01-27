cd9c707420239eeac13ce290542496fa3cd0d3a0
Leo Tang <litang@linkedin.com>
2015-01-27 00:05:28 -0800

    add TestNG dependency to NOTICE, fix tyle check error

--- expr/src/main/java/com/linkedin/featurefun/expr/Expression.java
+++ expr/src/main/java/com/linkedin/featurefun/expr/Expression.java
@@ -1,8 +1,17 @@
 /*
- * Copyright [2015] LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2015 LinkedIn Corp.
  *
- * Unless required by applicable law or agreed to in writing, software  distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software  
+ * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
+
 package com.linkedin.featurefun.expr;
 
 import java.util.List;
@@ -145,15 +154,15 @@ public class Expression implements Expr {
       builder.append(SExprTokenizer.CLOSE_PAREN);
     }
     return builder.toString();
-    }
+  }
 
     /***
      * Command line tool for testing and pretty printing expressions
      * @param args
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        if(args.length<1){
+        if (args.length < 1) {
             System.out.println("s-expression expected");
             return;
         }
@@ -161,10 +170,10 @@ public class Expression implements Expr {
         VariableRegistry registry = new VariableRegistry();
         Expr expr = Expression.parse(args[0], registry);
 
-        System.out.println("="+expr.toString());
+        System.out.println("=" + expr.toString());
 
-        if(registry.isEmpty()){
-            System.out.println("="+expr.evaluate());
+        if (registry.isEmpty()) {
+            System.out.println("=" + expr.evaluate());
         }
 
         System.out.println("tree");
